1a92e795be927bbd4ab24610b8eac46c9d4bd670
Corey Gilbert <coreyraygilbert@gmail.com>
2022-11-13 16:35:13 -0500

    Implimented Car Class

--- CodeLouisvilleUnitTestProject/Car.cs
+++ CodeLouisvilleUnitTestProject/Car.cs
@@ -1,22 +1,26 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit.Abstractions;
 using static System.Net.WebRequestMethods;
 
 namespace CodeLouisvilleUnitTestProject
 {
     public class Car : Vehicle
     {
-        private HttpClient httpClient = new HttpClient()
-        {
-            BaseAddress = new Uri(" https://vpic.nhtsa.dot.gov/api/") 
-        };
+        private ITestOutputHelper _logger;
 
+        private HttpClient _httpClient = new HttpClient()
+        {
+            BaseAddress = new Uri("https://vpic.nhtsa.dot.gov/api/") 
+        };
+        
         public new int NumberOfTires = 4;
         
-        public int numberOfPassengers { get; private set; }
+        public int NumberOfPassengers { get; private set; }
 
         public Car()
             : this(0, "", "", 0)
@@ -30,5 +34,82 @@ namespace CodeLouisvilleUnitTestProject
             Model = model;
             MilesPerGallon = milesPerGallon;          
         }
+
+        public async Task<bool> IsValidModelForMakeAsync()
+        {
+            bool isValidForMake = false;
+            try
+            {
+                var getUrl = $"{_httpClient.BaseAddress}/vehicles/GetModelsForMake/{this.Make}?format=json";
+                var response = await _httpClient.GetAsync(getUrl);
+                var respMessage = await response.EnsureSuccessStatusCode().Content.ReadAsStringAsync();
+
+                List<MakeModelSpecs> makeModelSpecs = JsonConvert.DeserializeObject<List<MakeModelSpecs>>(respMessage);
+
+                for (int i =0; i < makeModelSpecs.Count; i++)
+                {
+                    if (makeModelSpecs[i].ModelName == this.Model)
+                    {
+                        isValidForMake = true;
+                    }
+                } 
+            }
+            catch (Exception ex)
+            {
+                _logger.WriteLine("Failed when calling API to get MakeModel specs", ex);
+                return false;
+            }
+            return isValidForMake;
+            
+        }
+
+        public async Task<bool> WasModelMadeInYearAsync( int year)
+        {
+            bool wasModelMadeInYear = false;
+            if (year < 1995)
+            {
+                throw new ArgumentException("No data available for vehicles before year 1995");
+            }
+            try
+            {
+                var getUrl = $"{_httpClient.BaseAddress}/vehicles/GetModelsForMakeYear/make/{this.Make}/modelyear/{year}?format=json";
+                var response = await _httpClient.GetAsync(getUrl);
+                var respMessage = await response.EnsureSuccessStatusCode().Content.ReadAsStringAsync();
+                List<MakeModelSpecs> makeModelSpecs = JsonConvert.DeserializeObject<List<MakeModelSpecs>>(respMessage);
+                for (int i = 0; i < makeModelSpecs.Count; i++)
+                {
+                    if (makeModelSpecs[i].ModelName == this.Model)
+                    {
+                        wasModelMadeInYear = true;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.WriteLine("Failed when calling API to get MakeModel specs by Make and Year", ex);
+                return false;
+            }
+            return wasModelMadeInYear;
+        }
+            
+        public void AddPassengers(int numberOfPassengersToAdd)
+        {
+            NumberOfPassengers += numberOfPassengersToAdd;            
+            MilesPerGallon -= numberOfPassengersToAdd * .2;
+            if (MilesPerGallon < 0)
+            {
+                MilesPerGallon = 0;
+            }
+        }
+
+        public void RemovePassengers(int numberOfPassengersToRemove)
+        {
+            if (numberOfPassengersToRemove > NumberOfPassengers)
+            {
+                numberOfPassengersToRemove = NumberOfPassengers;
+            }            
+            NumberOfPassengers -= numberOfPassengersToRemove;
+            MilesPerGallon -= (numberOfPassengersToRemove * .2);
+        }   
     }
 }
---
+++ CodeLouisvilleUnitTestProject/MakeModelSpecs.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeLouisvilleUnitTestProject
+{
+    [Serializable]
+    public class MakeModelSpecs
+    {
+        [JsonProperty("Make_ID")]
+        public int MakeId { get; set; }
+
+        [JsonProperty("Make_Name")]
+        public string MakeName { get; set; }
+
+        [JsonProperty("Model_ID")]
+        public int ModelId { get; set; }
+
+        [JsonProperty("Model_Name")]
+        public string ModelName { get; set; }
+    }
+}
